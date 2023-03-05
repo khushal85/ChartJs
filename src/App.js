ff4f3724609d082e17495551fffbc4bde66ffc95
khushal singh <khushalsingh9998@gmail.com>
2023-03-05 23:29:24 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
-import logo from './logo.svg';
 import './App.css';
+import BarChart from './Charts/BarChart';
+import DoughnutChart from './Charts/DoughnutChart';
+import LineChart from './Charts/LineChart';
+import PieChart from './Charts/PieChart';
+
 
 function App() {
   return (
+  
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h2>BAR CHART</h2>
+      <BarChart />
+      <h2>DOUGHNUT CHART</h2>
+      <DoughnutChart />
+      <h2>LINE CHART</h2>
+      <LineChart />
+      <h2>PIE CHART</h2>
+     <PieChart />    
     </div>
+    
   );
 }
 
---
+++ src/Charts/DoughnutChart.js
@@ -0,0 +1,91 @@
+import React , {useState, useEffect} from 'react'
+import {Chart as ChartJS, ArcElement, Tooltip, Legend} from 'chart.js' 
+import { Doughnut } from  'react-chartjs-2'
+
+ ChartJS.register(
+    Tooltip,
+    Legend,
+    ArcElement,
+ );
+
+const DoughnutChart = () => {
+
+    const [chart,setChart]=useState([])
+
+    var baseUrl= "https://api.coinranking.com/v2/coins?limit=10"
+    var proxyUrl = 'https://cors-anywhere.herokuapp.com/'
+    var apiKey = "coinranking8f75c71a36414a684b0d2c54e0fb81af40e7d0e2213e9a8b"
+
+useEffect(() => {
+    const fetchCoins = async () => {
+        await fetch(`${proxyUrl}${baseUrl}`,{
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json',
+                'x-access-token': `${apiKey}`,
+                'Access-Control-Allow-Origin':'*'
+            }
+        }).then((Response) => {
+            Response.json().then((json) => {
+               console.log(json) 
+               setChart(json.data)
+            })
+        }).catch(error => {
+            console.log(error);
+        })
+    }
+    fetchCoins()
+},[baseUrl,proxyUrl,apiKey])
+
+console.log("chart", chart);
+ 
+var data = {
+      labels: chart?.coins?.map(x => x.name),
+      datasets: [{
+        label: `${chart?.coins?.length} Coins Available`,
+        data: chart?.coins?.map(x => x.price),
+        backgroundColor:[
+            'rgba(255,99,132,0.2)',
+            'rgba(54,162,235,0.2)',
+            'rgba(255,206,86,0.2)',
+            'rgba(75,192,192,0.2)',
+            'rgba(153,102,255,0.2)',
+            'rgba(255,159,64,0.2)'
+        ],
+        borderColor: [
+        'rgb(255,99,132)',
+        'rgb(54,162,235)',
+        'rgb(255,206,86)',
+        'rgb(75,192,192)',
+        'rgb(153,102,255)',
+        'rgb(255,159,64)'
+        ],
+        borderWidth: 1
+      }]
+    }
+var options = {
+    maintainAspectRatio: false,
+      scales: {
+        y: {
+          beginAtZero: true
+        }
+      },
+      legend:{
+        labels:{
+            fontSize:26
+        }
+      }
+    }
+
+  return (
+    <div>
+    <Doughnut
+    data ={data}
+    height={400}
+    options={options}
+    />
+    </div>
+  )
+}
+
+export default DoughnutChart
